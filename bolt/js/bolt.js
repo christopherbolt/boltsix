d08adb1429881dcdf34f1d4adedc36478a76f980
christopherbolt <chris@christopherbolt.com>
2024-09-12 14:45:20 +1200

    Update bolt.js

--- bolt/js/bolt.js
+++ bolt/js/bolt.js
@@ -10,7 +10,7 @@ if (self === top && /(christopherbolt\.com|boltdevserver\.local)$/.test(window.l
 	
 	$(document).ready(function() {
 		
-		$('form#frmCheckout input#accepttos').parents('.text-center').prepend('<p><strong>You must not use these services to send spam, unsolicted electronic messages or for any illegal purpose<br> You must comply with the Unsolicited Electronic Messages Act, the Privacy Act and all other applicable laws</strong></p><p>We take the security of our services seriously, all orders are manually reviewed, you do not need to do anything, but please allow a couple of hours for account activation. If you choose to pay by Bank Transfer your services will not be activated until payment has cleared.</p>');
+		$('form#frmCheckout input#accepttos').parents('.text-center').prepend('<p><strong>You must not use these services to send spam, unsolicted electronic messages or for any illegal purpose.<br> You must comply with the Unsolicited Electronic Messages Act, the Privacy Act and all other applicable laws.</strong></p><p>We take the security of our services seriously, all orders are manually reviewed, you do not need to do anything, but please allow a couple of hours for account activation.<br>If you choose to pay by Bank Transfer your services will not be activated until payment has cleared.</p>');
 		
 		if ($('.breadcrumb a[href*="BoltMail-Email-Marketing"]').length) {
 			$('.kb-article-details').prepend('<li style="margin-bottom: 1em">This article is about our <a href="https://www.boltmail.nz">email marketing platform</a> called BoltMail. You can learn more about this tool at <a href="https://www.boltmail.nz">boltmail.nz</a></li>');
